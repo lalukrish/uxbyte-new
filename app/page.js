68815c61037186bb8fdef5851d337c8ac3129297
lalukrish <lalukrishnavallil01@gmail.com>
2026-01-23 11:12:06 +0530

    page

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
       {/* <AnimatedHeader /> */}
       <TopHeader />
       <DamnGoodHero />
-      {/* <HeroNew /> */}
+      <HeroNew />
       <AboutSection />
       <ScrollingBox />
       <SaaSSection />
